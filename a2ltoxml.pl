7f45ed332da9a78201183c87ca73fb487d79344b
caesarhao <caesarhao@gmail.com>
2016-06-06 23:33:23 +0200

    it works

--- a2ltoxml.pl
+++ a2ltoxml.pl
@@ -1,2 +1,15 @@
 #!/usr/bin/env perl
-#
+# use local defined library.
+use lib qw(./lib/);
+use A2L;
+use strict;
+use warnings;
+
+print "Please drag in the input a2l file:";
+my $ifile = <STDIN>;
+$ifile =~ s///g;
+chomp $ifile;
+my $a2l = new A2L($ifile);
+print "\nA2L version: ", $a2l->get_version(), "\n";
+$a2l->outputXML();
+
--- lib/A2L.pm
+++ lib/A2L.pm
@@ -3,6 +3,7 @@ package A2L;
 use strict;
 use warnings;
 use File::Basename;
+use Data::Dumper;
 
 sub new {
     my $class = shift;
@@ -26,9 +27,15 @@ sub _initialize {
 	open(my $fh, '<', $self->{if_fullpath}) or die "Could not open file '$self->{if_fullpath}' $!";
 	$self->{if_path} = $dirs;
 	$self->{if_basename} = $filename;
+	local $/ = "\r\n";
 	while (my $row = <$fh>) {
     	chomp $row;
     	push $self->{lines}, $row;
+    	if ($row =~ /^ASAP2_VERSION\ .*/){
+  			my @words = split / /, $row;
+  			shift @words;
+  			$self->set_version(join(' ', @words));
+  		}
   	}
   	close($fh);
 }
@@ -48,32 +55,50 @@ sub outputXML {
     my $self = shift;
     my $outputXml = $self->{if_path} . $self->{if_basename} . ".xml";
     open(my $fh, '>', $outputXml) or die "Could not open file '$outputXml' $!";
+    print $fh "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
     foreach my $row (@{$self->{lines}}) {
-    	if ($_ =~ /\s*\/begin\ .*/){
-    		my $pos_begin = index $_, "/begin";
-    		for (my $i = 0; $i < $pos_begin; $i++) {
-       			print " ";
-    		}
-    		my @words = split / /, (substr $str, ($pos_begin + 6));
-    		print $fh "<" . $words[0];
+    	chomp($row);
+    	$row =~ s/\"/&quot;/g;
+    	if ($row =~ /ASAP2_VERSION\ .*/){
+    		my @words = split / /, $row;
+    		print $fh "<?", $words[0];
     		shift @words;
-    		if (scalar(@words) > 1){
-    			print $fh " property=\"".join( ' ', @words)."\">";
+    		if (scalar(@words) > 0){
+    			print $fh " property=\"", join(' ', @words), "\"?>";
+    		}
+    		else{
+    			print $fh "?>";
+    		}
+    	}
+    	elsif ($row =~ /\s*\/begin\ .*/){
+    		my $pos_begin = index $row, "\/begin";
+    		for (my $i = 0; $i < $pos_begin; $i++) {
+       			print $fh " ";
+    		}
+    		my @words = split / /, (substr $row, $pos_begin);
+    		# print Dumper(@words);
+    		shift @words;
+    		print $fh "<", $words[0];
+    		shift @words;
+    		if (scalar(@words) > 0){
+    			print $fh " property=\"", join(' ', @words), "\">";
     		}
     		else{
     			print $fh ">";
     		}
+    		
     	}
-    	elsif ($_ =~ /\s*\/end\ .*/){
-    		my $pos_end = index $_, "/end";
+    	elsif ($row =~ /\s*\/end\ .*/){
+    		my $pos_end = index $row, "\/end";
     		for (my $i = 0; $i < $pos_end; $i++) {
-       			print " ";
+       			print $fh " ";
     		}
-    		my @words = split / /, (substr $str, ($pos_end + 6));
-    		print $fh "</" . $words[0] . ">";
+    		my @words = split / /, (substr $row, $pos_end);
+    		shift @words;
+    		print $fh "<\/", $words[0], ">";
     	}
     	else{
-    		print $fh $_;
+    		print $fh $row;
     	}
     	print $fh "\n";
     }
